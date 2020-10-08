2301944f77ed9538705e9f8b84410b6ebe4cb255
Tim Wasinger (aka SteveBeeblebrox) <41750582+SteveBeeblebrox@users.noreply.github.com>
2020-10-08 12:49:00 -0600

    Fixed a bug with detecting proper properties
    (Similar cause and fix to the last bug, but in a different place)

--- shml.js
+++ shml.js
@@ -49,7 +49,7 @@ function parseMD(markdown = '', properties = []) {
     else {
       for(var property of properties)
         if(data.properties[property] === undefined)
-          object.replace(new RegExp('\s*' + property + ':(.*)'), (str, match) => (data.properties[property] = match.trim(), ''));
+          object.replace(new RegExp('^\\s*?' + property + ':(.*)'), (str, match) => (data.properties[property] = match.trim(), ''));
 
       for(var i = 1; i < 7; i++) parseForSection('h' + i, object);
       parseForSection('p', object);
