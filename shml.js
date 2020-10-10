e6f475930312acd1788b380c7b1cbdcbcb47047e
Tim Wasinger (aka SteveBeeblebrox) <41750582+SteveBeeblebrox@users.noreply.github.com>
2020-10-10 14:30:35 -0600

    Namespace cleanup
    Renamed parseMD to parseMarkup and made it a function of the SHML object

--- shml.js
+++ shml.js
@@ -1,19 +1,14 @@
 /*
-
 MIT License
-
 Copyright (c) 2020 SteveBeeblebrox
-
 Permission is hereby granted, free of charge, to any person obtaining a copy
 of this software and associated documentation files (the "Software"), to deal
 in the Software without restriction, including without limitation the rights
 to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
 copies of the Software, and to permit persons to whom the Software is
 furnished to do so, subject to the following conditions:
-
 The above copyright notice and this permission notice shall be included in all
 copies or substantial portions of the Software.
-
 THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
 IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
 FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
@@ -21,44 +16,44 @@ AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
 LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE.
-
 */
+SHML = {
+  parseMarkup: function(markdown = '', properties = []) {
+    let data = {
+      properties: {},
+      stack: [],
+      toHTML: () => data.stack.join(''),
+      getProperty: (property) => data.properties[property]
+    };
+    let push = object => data.stack.push(object);
+    let parseForInlineFormatting = str =>
+      str
+      .replace(/(\*\*\*)(.*?)\1/gs, '<strong><i>$2</i></strong>')
+      .replace(/(\*\*)(.*?)\1/gs, '<strong>$2</strong>')
+      .replace(/(\*)(.*?)\1/g, '<i>$2</i>')
+      .replace(/(__)(.*?)\1/g, '<u>$2</u>')
+      .replace(/(~~)(.*?)\1/g, '<del>$2</del>')
+      .replace(/(\^)(.*?)\1/g, '<sup>$2</sup>')
+      .replace(/(\|)(.*?)\1/g, '<mark>$2</mark>')
+      .replace(/(`)(.*?)\1/g, '<code>$2</code>')
+      .replace(/\[(.*?)\]\((.*?)\)/g, '<a href="$2" title="$1" target="_blank">$1</a>')
+      .replace(/%%/g, '<br>')
+      ;
+    let parseForSection = (tag, str, key = tag) => str.replace(new RegExp('^\\s*?' + key + ':(.*)', 'g'), (str, match) => (push('<' + tag + '>' + parseForInlineFormatting(match.trim()) + '</' + tag + '>'), ''));
+    markdown.split(/\n/g).forEach((object, index, array) => {
+      if(object.trim().startsWith('<') && object.trim().endsWith('>')) push(object);
+      else {
+        for(var property of properties)
+          if(data.properties[property] === undefined)
+            object.replace(new RegExp('^\\s*?' + property + ':(.*)'), (str, match) => (data.properties[property] = match.trim(), ''));
 
-function parseMD(markdown = '', properties = []) {
-  let data = {
-    properties: {},
-    stack: [],
-    toHTML: () => data.stack.join(''),
-    get: (property) => data.properties[property]
-  };
-  let push = object => data.stack.push(object);
-  let parseForInlineFormatting = str =>
-    str
-    .replace(/(\*\*\*)(.*?)\1/gs, '<strong><i>$2</i></strong>')
-    .replace(/(\*\*)(.*?)\1/gs, '<strong>$2</strong>')
-    .replace(/(\*)(.*?)\1/g, '<i>$2</i>')
-    .replace(/(__)(.*?)\1/g, '<u>$2</u>')
-    .replace(/(~~)(.*?)\1/g, '<del>$2</del>')
-    .replace(/(\^)(.*?)\1/g, '<sup>$2</sup>')
-    .replace(/(\|)(.*?)\1/g, '<mark>$2</mark>')
-    .replace(/(`)(.*?)\1/g, '<code>$2</code>')
-    .replace(/\[(.*?)\]\((.*?)\)/g, '<a href="$2" title="$1" target="_blank">$1</a>')
-    .replace(/%%/g, '<br>')
-    ;
-  let parseForSection = (tag, str, key = tag) => str.replace(new RegExp('^\\s*?' + key + ':(.*)', 'g'), (str, match) => (push('<' + tag + '>' + parseForInlineFormatting(match.trim()) + '</' + tag + '>'), ''));
-  markdown.split(/\n/g).forEach((object, index, array) => {
-    if(object.trim().startsWith('<') && object.trim().endsWith('>')) push(object);
-    else {
-      for(var property of properties)
-        if(data.properties[property] === undefined)
-          object.replace(new RegExp('^\\s*?' + property + ':(.*)'), (str, match) => (data.properties[property] = match.trim(), ''));
-
-      for(var i = 1; i < 7; i++) parseForSection('h' + i, object);
-      parseForSection('p', object);
-      object.replace(/^\s*?(?:bull:|\+)(.*)/g, (str, match) => (push('<ul><li>' + parseForInlineFormatting(match.trim()) + '</li></ul>'), ''));
-      object.replace(/\s*---+\s*/, () => (push('<hr>'), ''));
-      object.replace(/\s*%%\s*/, () => (push('<br>'), ''));
-    }
-  });
-  return data;
+        for(var i = 1; i < 7; i++) parseForSection('h' + i, object);
+        parseForSection('p', object);
+        object.replace(/^\s*?(?:bull:|\+)(.*)/g, (str, match) => (push('<ul><li>' + parseForInlineFormatting(match.trim()) + '</li></ul>'), ''));
+        object.replace(/\s*---+\s*/, () => (push('<hr>'), ''));
+        object.replace(/\s*%%\s*/, () => (push('<br>'), ''));
+      }
+    });
+    return data;
+  }
 }
