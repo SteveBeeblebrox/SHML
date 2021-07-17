8f748beb3334ed920ec104406e77c02c62a165f2
T Wasinger <41750582+SteveBeeblebrox@users.noreply.github.com>
2021-07-16 21:48:06 -0600

    Update shml-file-manager.ts

--- shml-file-manager.ts
+++ shml-file-manager.ts
@@ -26,37 +26,43 @@ type StringObjectCollection = {
   [key: string]: string
 }
 
-type SHMLFileMetadata = {contents: string, flavor?: string, version?: string, encryption?: string}
+type SHMLFileMetadata = {contents: string, flavor: string, version: string, encryption: string}
 
-type CryptographyFunction = {(text: string, password: string): Promise<string>}
-  
-type SHMLEncryptionTypes = {
-    [key: string]: {
-        encrypt: CryptographyFunction,
-        decrypt: CryptographyFunction
+abstract class AbstractSHMLSource {
+    static readonly NONE: string = 'NONE'
+
+    contents: string
+    flavor: string = 'SHML'
+    version: string = '1.0.0'
+    constructor({contents, flavor = 'SHML', version = '1.0.0'}: {contents: string, flavor: string, version: string}) {
+            this.contents = contents
+            const metadata: StringObjectCollection = {flavor, version}
+            Object.keys(metadata).forEach(SHMLSource.validateProperty);
+            Object.assign(this, metadata);
+    }
+    static validateProperty(property: string) {
+        if(/[:\s]/.test(property)) throw `Illegal metadata value "${property}" for property "${property}". Whitespace and semicolons are not allowed.`
     }
 }
 
-class SHMLFile {
-    isEncrypted(): boolean {
-        return this.encryption !== 'NONE'
+class SHMLFile extends AbstractSHMLSource {
+
+    encryption: string =  AbstractSHMLSource.NONE
+    constructor({contents, flavor = 'SHML', version = '1.0.0', encryption = AbstractSHMLSource.NONE}: SHMLFileMetadata) {
+        super({contents, flavor, version})
+        SHMLSource.validateProperty(encryption)
+        this.encryption = encryption
     }
+
     toString(): string {
         return `${this.flavor}::${this.version}::${this.encryption}\n${this.contents}`
     }
-    public contents?:string
-    public flavor?:string
-    public version?:string
-    public encryption?: string
-    constructor({contents, flavor = 'SHML', version = '1.0.0', encryption = 'NONE'}: SHMLFileMetadata) { 
-        const metadata: StringObjectCollection = {flavor, version, encryption}
-        Object.keys(metadata).forEach(property => {
-            if(/[:\s]/.test(metadata[property])) throw `Illegal metadata value "${metadata[property]}" for property "${property}". Whitespace and semicolons are not allowed.`
-        });
-        Object.assign(this, {contents, ...metadata})
+
+    isEncrypted(): boolean {
+        return this.encryption !== 'NONE'
     }
 
-    static async fromString(source: string): Promise<any> {
+    static async fromString(source: string): Promise<SHMLFile> {
         return new Promise((resolve, reject) => {
             source.replace(/(?:(?<metadata>[^:\s])){0}^(?<flavor>[^:\s]*?)::(?<version>[^:\s]*?)::(?<encryption>[^:\s]*?)(?:\r\n|\n)(?<contents>[\s\S]*)$/, function(...args: any[]): string {
                 const map: SHMLFileMetadata = args.pop()
@@ -70,14 +76,47 @@ class SHMLFile {
     }
 }
 
-class SHMLFileCryptographyHandler {
+class SHMLSource extends AbstractSHMLSource {
+
+}
+
+type SHMLResult = {
+}
+
+
+
+type CryptographyFunction = {(text: string, password: string): Promise<string>}
+  
+type SHMLEncryptionTypes = {
+    [key: string]: {
+        encrypt: CryptographyFunction,
+        decrypt: CryptographyFunction
+    }
+}
+
+type ParserFunction = {
+    (markup: string, ...args: any): SHMLResult
+}
+type VersionMapping = {
+    [flavor: string]: {
+        (version: string): ParserFunction | null
+    }
+}
+
+type Predicate<T> = {
+    (value: T): boolean
+}
+
+
+
+class SHMLFileManager {
     private encryptionTypes: SHMLEncryptionTypes
     private static simpleResolver = <T,>() => (value: T) => new Promise(resolve => resolve(value)) as Promise<T>
-    constructor(encryptionTypes: SHMLEncryptionTypes = {}) {
+    constructor(public versionMapping: VersionMapping, encryptionTypes: SHMLEncryptionTypes = {}) {
         this.encryptionTypes = {
             'NONE': {
-                encrypt: SHMLFileCryptographyHandler.simpleResolver<string>(),
-                decrypt: SHMLFileCryptographyHandler.simpleResolver<string>()
+                encrypt: SHMLFileManager.simpleResolver<string>(),
+                decrypt: SHMLFileManager.simpleResolver<string>()
             },
             ...encryptionTypes
         }
@@ -87,22 +126,40 @@ class SHMLFileCryptographyHandler {
         return Object.keys(this.encryptionTypes).includes(file.encryption!)
     }
 
-    public async decrypt(file: SHMLFile, password: string): Promise<SHMLFile> {
+    public canSupportSource(source: AbstractSHMLSource): boolean {
+        return source.flavor! in this.versionMapping && this.versionMapping[source.flavor!](source.version!) !== null
+    }
+
+    public async decrypt(file: SHMLFile, password: string = ''): Promise<SHMLSource> {
         if(!this.canDecryptFile(file)) throw `Unsupported encryption type "${file.encryption}"`
-        return new SHMLFile({
+        return new SHMLSource({
             contents: await this.encryptionTypes[file.encryption!].decrypt(file.contents!, password),
             flavor: file.flavor,
-            version: file.version,
-            encryption: 'NONE'
+            version: file.version
         })
     }
 
-    public async ecrypt(file: SHMLFile, password: string, encryption: string = 'NONE'): Promise<SHMLFile> {
+    public async encrypt(file: SHMLSource, encryption: string = 'NONE', password: string = ''): Promise<SHMLFile> {
         return new SHMLFile({
             contents: await this.encryptionTypes[encryption].encrypt(file.contents!, password),
             flavor: file.flavor,
             version: file.version,
             encryption: encryption
         })
+    }
+
+    public async fromFile(source: string | SHMLFile, password: string = ''): Promise<SHMLSource> {
+        if(typeof source === 'string' || source instanceof String) source = await SHMLFile.fromString(source as string)
+        if(!this.canSupportSource(source)) throw 'Unsupported file type or version'
+        return await this.decrypt(source, password)
+    }
+
+    public async toFile(source: SHMLSource, encryption: string = AbstractSHMLSource.NONE, password: string = ''): Promise<SHMLFile> {
+        return this.encrypt(source, encryption, password)
+    }
+
+    public async parse(source: SHMLSource): Promise<string> {
+        if(!this.canSupportSource(source)) throw 'Unsupported file type or version'
+        return (this.versionMapping[source.flavor](source.version) ?? (function() {return ''}))(source.contents) as string;
     }
 }
