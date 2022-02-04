69af227cde87824c0708273c8201ed03c54e8db7
T Wasinger <41750582+SteveBeeblebrox@users.noreply.github.com>
2022-02-03 20:16:33 -0700

    Bug fix

--- shml.js
+++ shml.js
@@ -23,7 +23,7 @@
  */
 var SHML;
 (function (SHML) {
-    SHML.VERSION = '1.3.2';
+    SHML.VERSION = '1.3.3';
     function cyrb64(text, seed = 0) {
         let h1 = 0xdeadbeef ^ seed, h2 = 0x41c6ce57 ^ seed;
         for (let i = 0, ch; i < text.length; i++) {
@@ -230,7 +230,7 @@ var SHML;
                     const overrides = { centered: 'center', justified: 'justify', 'justified-all': 'justify-all' };
                     return `<div style="text-align: ${(_a = overrides[groups.what]) !== null && _a !== void 0 ? _a : groups.what};">${groups.TEXT}</div>`;
                 } });
-            args.set('numbered_header', { pattern: /^\s*?(?<count>#{1,6})(?:\[(?<id>[a-zA-Z_][a-zA-Z_0-9]*?)\])?\s?(?<TEXT>[^\uffff]*?)(?=\n)/gm, isInline: false, reviver({ groups }) {
+            args.set('numbered_header', { pattern: /^\s*?(?<count>#{1,6})(?:\[(?<id>[a-zA-Z_][a-zA-Z_0-9]*?)\])?\s?(?<TEXT>[^\uffff]*?)(?=\n|$)/gm, isInline: false, reviver({ groups }) {
                     var _a;
                     if (groups.id)
                         ids.add(`h${groups.count.length}:${groups.id}`);
